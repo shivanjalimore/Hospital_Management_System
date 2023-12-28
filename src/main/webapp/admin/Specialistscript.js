32b77a8df8d10e98977c2853d20cbdb5a5356154
Shivanjali More <shivanjalimore035@gmail.com>
2023-12-28 20:52:14 +0530

    Update Specialistscript.js

--- src/main/webapp/admin/Specialistscript.js
+++ src/main/webapp/admin/Specialistscript.js
@@ -8,20 +8,9 @@ document.getElementById('closeBtn').addEventListener('click', function () {
     document.getElementById('specialistPopup').style.display = 'none';
 });
 
-// Functionality for adding a specialist (Add button)
-//document.getElementById('addSpecialistBtn').addEventListener('click', function () {
-    // Here, you can add your logic to handle adding a specialist
-   // let specialistName = document.getElementById('specialistInput').value;
-    // Perform actions with the specialist name (e.g., send to the server)
-    //console.log('Added specialist:', specialistName);
-    // Clear input field or perform other actions if needed
-    //document.getElementById('specialistInput').value = '';
-//});
-
-// Functionality for saving changes (Save Changes button)
 document.getElementById('saveChangesBtn').addEventListener('click', function () {
-    // Here, you can add your logic to save any changes made in the popup
-    //console.log('Changes saved.');
-    
     document.getElementById('specialistPopup').style.display = 'none';
 });
+
+
+    
